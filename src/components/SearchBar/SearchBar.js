0967d9e1d2a30b582bb0f07bb3ada0ca88fe94f5
Gerardo Arturo Miranda <gerardomg015@gmail.com>
2022-05-31 22:16:33 -0500

    Changes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { useCallback } from 'react'
 export default function SearchBar({ pokemonName, onNameChange }) {
 
   const handleInputChange = useCallback(event => {
     onNameChange(event.target.value)
   }, [onNameChange])
+
   return (
     <div className='row w-100'>
       <input onChange={handleInputChange} value={pokemonName}></input>
